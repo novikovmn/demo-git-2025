70fc9158cabcf60434a099afdc88a371a5f77205
novikovmn <novikovmn93@gmail.com>
2025-10-23 17:50:54 +0300

    init commit

---
+++ src/main/java/by/novikov/mn/demogit2025/DemoGit2025Application.java
@@ -0,0 +1,13 @@
+package by.novikov.mn.demogit2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGit2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGit2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/by/novikov/mn/demogit2025/DemoGit2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.novikov.mn.demogit2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGit2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
