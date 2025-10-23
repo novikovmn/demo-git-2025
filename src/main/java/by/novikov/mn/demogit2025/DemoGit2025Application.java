a70438efdd895cc620c518770c2091104491a235
novikovmn <novikovmn93@gmail.com>
2025-10-23 17:58:55 +0300

    test commit

--- src/main/java/by/novikov/mn/demogit2025/DemoGit2025Application.java
+++ src/main/java/by/novikov/mn/demogit2025/DemoGit2025Application.java
@@ -8,6 +8,7 @@ public class DemoGit2025Application {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoGit2025Application.class, args);
+        System.out.println("Hello World!");
     }
 
 }
